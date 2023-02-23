08f22e04956ad19a6a755685b110a7dec29e9fca
Antonio Pisanello <antoniopisanello@Antonios-MacBook-Pro.local>
2023-02-23 09:03:20 +0100

    add WTForms

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from functools import wraps
+from flaskweek_functions import authenticate
+from flaskweek_functions.forms import ExampleForms, UserLogin
+
+from wtforms import Form, validators, StringField, PasswordField
+
+class loginWTF(Form):
+    uid = StringField("Email or username", validators=[validators.InputRequired(), validators.Length(min=5, max=15)])
+    pwd = PasswordField("Password", validators=[validators.InputRequired(), validators.equal_to('pwd2', message='PASSWORDS DONT MATCH')])
+    pwd2 = PasswordField("confirm password", validators=[validators.InputRequired()])
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
-
-def authenticate(f):
-    @wraps(f) 
-    def decorated_func(*args, **kwargs):
-        if "loggedin" in session and session["loggedin"]:
-            return f(*args, **kwargs)
-        else:
-            return redirect(url_for("login"))
-    return decorated_func
 
 @app.route("/")
 def index():
@@ -39,10 +38,11 @@ def signup():
     else : # when GET
         return render_template("login/signup.html.j2")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST", "PUT"])
 def login():
     if "loggedin" in session and session["loggedin"]:
         return redirect("secure")
+
 
     if request.method == "POST":
         uid = request.form.get("uid", "")
@@ -54,8 +54,35 @@ def login():
             session["uid"] = uid
 
             return redirect(url_for("secure"))
+
         
     return render_template("login/login.html.j2")
+
+
+@app.route("/loginwtf", methods=["GET", "POST"])
+def loginwtf():
+    formLoginWTF = loginWTF(request.form)
+
+    if request.method == "POST" and formLoginWTF.validate():
+        uid = formLoginWTF.uid.data
+        pwd = formLoginWTF.pwd.data
+        pwd2 = formLoginWTF.pwd2.data
+
+        if len(uid) < 5 or len(uid) > 15 or not pwd == pwd2:
+            # error
+            pass 
+        print(uid)
+        print(pwd)
+
+        if uid == "anto" and pwd == "1234":
+            session["loggedin"] = True
+            session["uid"] = uid
+
+            return redirect(url_for("secure"))
+
+    return render_template("login/login.html.j2", form=formLoginWTF)
+    
+    
 
 @app.route("/logout")
 @authenticate
@@ -67,6 +94,11 @@ def logout():
 @authenticate
 def secure():
     return render_template("secure.html.j2")
+
+@app.route("/form_example")
+def form_example():
+    form = ExampleForms(request.form)
+    return render_template("forms/forms_without_macro.html.j2", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
